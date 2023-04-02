36b19fe21e4591bacb7f938909c3e560bb20aad2
Evgeny Yarusov <yarusovea@gmail.com>
2023-04-02 17:15:57 +0500

    HeaderTestsForUnloggedUser is created and validated

---
+++ src/test/java/com/PhoneBook/HeaderTestsForUnloggedUser.java
@@ -0,0 +1,51 @@
+package com.PhoneBook;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HeaderTestsForUnloggedUser extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!isElementPresentByLocator(
+                By.cssSelector("div.navbar-component_nav__1X_4m a[href='/login']"))
+        ) {
+            driver.findElement(
+                    By.xpath("//button[contains(.,'Sign Out')]")
+            ).click();
+        }
+    }
+    @Test
+    public void checkHeaderLogoTest() {
+        Assert.assertTrue(
+                isElementPresentByLocator(
+                        By.xpath("//div[@class='navbar-component_nav__1X_4m']/h1[contains(.,'PHONEBOOK')]")
+                )
+        );
+    }
+    @Test
+    public void checkHeaderHomeLinkTest() {
+        Assert.assertTrue(
+                isElementPresentByLocator(
+                        By.cssSelector("div.navbar-component_nav__1X_4m a[href='/home']")
+                )
+        );
+    }
+    @Test
+    public void checkHeaderAboutLinkTest() {
+        Assert.assertTrue(
+                isElementPresentByLocator(
+                        By.cssSelector("div.navbar-component_nav__1X_4m a[href='/about']")
+                )
+        );
+    }
+    @Test
+    public void checkHeaderLoginLinkTest() {
+        Assert.assertTrue(
+                isElementPresentByLocator(
+                        By.cssSelector("div.navbar-component_nav__1X_4m a[href='/login']")
+                )
+        );
+    }
+}
